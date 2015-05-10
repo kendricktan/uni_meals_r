03826f10f4e1caa8560a6f280586a068af022250
Kendrick Tan <kendricktan0814@gmail.com>
2015-05-10 19:12:12 +1000

    uni_meals website revamp layout
    Revamped layout, essentially an empty shell right now

---
+++ static/js/uni-meals.js
@@ -0,0 +1,9 @@
+// Nav tab
+$(document).ready(function() {
+  	$('#tab-wizard').bootstrapWizard({'tabClass': 'nav nav-pills'});
+});
+
+// Tooltip
+$(function () {
+    $("[rel='tooltip']").tooltip();
+});
